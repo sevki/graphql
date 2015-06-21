4e822e6e00f8a378385161e9900265e8751edde7
Sevki Hasirci <s@sevki.org>
2015-06-21 19:38:56 +0300

    documentation

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2015 Sevki <s@sevki.org>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package graphql implements the graphql spesification  that
+// is yet to  be released. Details and the spec of the package are not
+// yet  public, once they are chances are they will show up here
+// https://sevki.co/2rmgfV
+package graphql // import "sevki.org/graphql"
