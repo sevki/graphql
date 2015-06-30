4f8f272ba9dd640d408daeaedc86f51da7072a5a
Sevki Hasirci <s@sevki.org>
2015-06-30 08:22:08 +0300

    doc

--- doc.go
+++ doc.go
@@ -2,8 +2,26 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package graphql implements the graphql spesification  that
-// is yet to  be released. Details and the spec of the package are not
-// yet  public, once they are chances are they will show up here
-// https://sevki.co/2rmgfV
+/*
+Package graphql implements the graphql specification  that
+is yet to  be released.
+
+What it is
+
+What GraphQL currently gives you is an abstract syntax tree after
+receiving which you need to figure out how to execute it, an example
+of such an application lives at https://sevki.org/g2sd.
+
+More info
+
+Details and the spec of the package are not yet  public, once they are
+chances are they will show up here https://sevki.co/2rmgfV.
+
+Facebook has written about this
+http://facebook.github.io/react/blog/2015/05/01/graphql-introduction.html
+talked about this https://www.youtube.com/watch?v=9sc8Pyc51uU.
+
+I've also written about on my blog https://ffbyt.es/C39Q
+
+*/
 package graphql // import "sevki.org/graphql"
